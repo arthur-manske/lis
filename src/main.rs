3cacfb2342491b982c026ce0178354c307f55cf7
Arthur Manske <130318764+arthur-manske@users.noreply.github.com>
2023-04-22 19:24:19 -0300

    Update main.rs
    Moved the functions of printing to the printer.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 mod arguments;
 mod printer;
 use crate::arguments::Arguments;
-use crate::printer::print_file;
-use colored::{Color, Colorize};
-use std::fs::DirEntry;
-const PATH_COLOR: Color = Color::BrightBlue;
-
+use crate::printer::printer;
+pub const VERSION: &str = "0.2.1";
 
 fn main() {
     let all_arguments: (Arguments, String) = Arguments {
@@ -18,12 +15,5 @@ fn main() {
     .interpreter();
     let arguments: Arguments = all_arguments.0;
     let path: String = all_arguments.1;
-    let entries: Vec<DirEntry> = arguments.analyze_entries(&path);
-    println!(
-        "The content of the directory: {} is:",
-        path.color(PATH_COLOR)
-    );
-    for entry in entries {
-        print_file(&entry, arguments.clone());
-    }
+    printer(arguments.analyze_entries(&path), arguments, path)
 }
