62fea43742def516d08f7ee6b3cb8e0087dc544c
Arthur Manske <130318764+arthur-manske@users.noreply.github.com>
2023-04-23 12:57:09 -0300

    Update arguments.rs

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,9 +1,10 @@
 use crate::printer::error;
 use crate::printer::helper;
+use crate::PROGRAM_NAME;
 use std::env;
 use std::fs;
 use std::fs::DirEntry;
-pub const VALID_ARGUMENTS: [&str; 7] = ["-a", "-p", "-d", "-h", "-sh", "-s", "-no"];
+pub const VALID_ARGUMENTS: [&str; 7] = ["-h", "-a", "-d", "-no", "-p", "-s", "-sh"];
 
 #[derive(Clone)]
 pub struct Arguments {
@@ -15,6 +16,15 @@ pub struct Arguments {
 }
 
 impl Arguments {
+    pub fn new() -> Self {
+        Arguments {
+            show_hidden: false,
+            perm: false,
+            date: false,
+            size: false,
+            no_ordering: false,
+        }
+    }
     pub fn analyze_entries(&self, path: &str) -> Vec<DirEntry> {
         let entries: Vec<_> = match fs::read_dir(path) {
             Ok(entries) => entries.filter_map(|entry| entry.ok()).collect(),
@@ -64,39 +74,37 @@ impl Arguments {
         }
         path
     }
-
     fn parse_arguments(mut self, argument: &str) -> Self {
         match VALID_ARGUMENTS.iter().position(|&x| x == argument) {
-            Some(0) => {
+            Some(0) => helper(),
+            Some(1) => {
                 self.perm = true;
                 self.date = true;
                 self.show_hidden = true;
                 self.size = true;
             }
-            Some(1) => self.perm = true,
             Some(2) => self.date = true,
-            Some(3) => helper(),
-            Some(4) => self.show_hidden = true,
+            Some(3) => self.no_ordering = true,
+            Some(4) => self.perm = true,
             Some(5) => self.size = true,
-            Some(6) => self.no_ordering = true,
+            Some(6) => self.show_hidden = true,
             _ => error(
                 "Invalid argument",
-                "type 'list -h' to see all the arguments",
+                &format!(
+                    "type '{} {}' to see all the arguments",
+                    PROGRAM_NAME, VALID_ARGUMENTS[0]
+                ),
             ),
         }
         self
     }
     pub fn interpreter(mut self) -> (Arguments, String) {
-        let arguments = env::args().skip(1);
+        let command_arguments = env::args().skip(1);
         let mut path: String = env::current_dir().unwrap().to_str().unwrap().to_owned();
-        for argument in arguments {
+        for argument in command_arguments {
             match !argument.starts_with('-') {
-                true => {
-                    path = self.parse_path(argument);
-                }
-                false => {
-                    self = self.parse_arguments(&argument);
-                }
+                true => path = self.parse_path(argument),
+                false => self = self.parse_arguments(&argument),
             }
         }
         (self, path)
