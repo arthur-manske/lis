970977a71630e6f017b79a17ce55f983eda1bfb9
Arthur Manske <130318764+arthur-manske@users.noreply.github.com>
2023-04-23 12:56:48 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,12 @@ mod arguments;
 mod printer;
 use crate::arguments::Arguments;
 use crate::printer::printer;
+pub const PROGRAM_NAME: &str = "list";
 pub const VERSION: &str = "0.2.1";
 
 fn main() {
-    let all_arguments: (Arguments, String) = Arguments {
-        show_hidden: false,
-        perm: false,
-        date: false,
-        size: false,
-        no_ordering: false,
-    }
-    .interpreter();
-    let arguments: Arguments = all_arguments.0;
-    let path: String = all_arguments.1;
+    let arguments = Arguments::new().interpreter();
+    let path = arguments.1;
+    let arguments = arguments.0;
     printer(arguments.analyze_entries(&path), arguments, path)
 }
