a62779765e4529c572f6cbbd2392b722cf7313d3
Arthur Manske <130318764+arthur-manske@users.noreply.github.com>
2023-04-25 19:04:16 -0300

    Update main.rs
    Made the variable declaration more simple and intuitive

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ pub const PROGRAM_NAME: &str = "list";
 pub const VERSION: &str = "0.2.1";
 
 fn main() {
-    let arguments = Arguments::new().interpreter();
-    let path = arguments.1;
-    let arguments = arguments.0;
+    let (arguments, path) = Arguments::new().interpreter();
     printer(arguments.analyze_entries(&path), arguments, path)
 }
