7e0516c1a9e0d37701738fd27280e3dee48870d8
Arthur Manske <130318764+arthur-manske@users.noreply.github.com>
2023-04-28 20:34:15 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod arguments;
 mod printer;
 use crate::arguments::Arguments;
 use crate::printer::printer;
-pub const PROGRAM_NAME: &str = "list";
-pub const VERSION: &str = "0.2.1";
+pub const PROGRAM_NAME: &str = "lis";
+pub const VERSION: &str = "0.2.2";
 
 fn main() {
     let (arguments, path) = Arguments::new().interpreter();
