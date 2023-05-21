7417b3ed5da4498a6d6d0ae66f04d4bc16884fbb
Arthur Manske <130318764+arthur-manske@users.noreply.github.com>
2023-05-21 13:25:03 -0300

    Refactored the arguments.rs file for improved code structure and readability.
    The arguments.rs file has been refactored to improve code structure and readability. The modifications include the following:
    
    Updated import statements:
    Imported the error, helper, and version functions from the print module.
    Updated import statements for args and current_dir from the std::env module.
    Updated import statements for read_dir and DirEntry from the std::fs module.
    Renamed and restructured the Arguments struct:
    Renamed the no_ordering field to no_order.
    Added a new field named force_printing_as_lines to represent the --lines option.
    Added default implementations for the Default trait to provide default field values.
    Updated method names:
    Renamed the show_hidden method to show_hidden() for consistency.
    Renamed the permissions method to permissions() for consistency.
    Renamed the date method to date() for consistency.
    Renamed the size method to size() for consistency.
    Renamed the no_order method to no_order() for consistency.
    Simplified the analyze_entries method:
    Updated the import statement for read_dir to match the new import statement.
    Simplified the code logic using match expressions for readability.
    Replaced the error handling with a call to std::process::exit and the error function.
    Simplified the parse_path method:
    Removed the unnecessary condition and directly appended '/' to the path if needed.
    Simplified the parse_arguments method:
    Updated the switch-case statement to use match expressions for improved readability.
    Replaced the error handling with a call to std::process::exit and the error function.
    Updated the interpreter method:
    Removed the mut self parameter as the Arguments struct is now Default.
    Simplified the code logic using match expressions and to_lowercase for consistency.
    Updated the import statement for args to match the new import statement.
    Simplified the code to initialize the arguments variable.
    Removed the explicit type annotations where unnecessary.
    Replaced the error handling with a call to std::process::exit and the error function.

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,146 +1,102 @@
-use crate::printer::error;
-use crate::printer::helper;
-use crate::PROGRAM_NAME;
-use std::env;
-use std::fs;
-use std::fs::DirEntry;
+use crate::print::{error, helper, version};
+use std::env::{args, current_dir};
+use std::fs::{read_dir, DirEntry};
 
-pub const VALID_ARGUMENTS: [&str; 6] = [
-    "--help",
-    "--date",
-    "--no-order",
-    "--permissions",
-    "--size",
-    "--show-hidden",
-];
-
-pub const VALID_ABREVIATIONS: [&str; 7] = ["-h", "-d", "-no", "-p", "-s", "-sh", "-a"];
-
-#[derive(Clone)]
+#[derive(Default)]
 pub struct Arguments {
-    pub show_hidden: bool,
-    pub perm: bool,
-    pub date: bool,
-    pub size: bool,
-    pub no_ordering: bool,
+    show_hidden: bool,
+    perm: bool,
+    date: bool,
+    size: bool,
+    no_order: bool,
+    force_print_as_lines: bool,
 }
 
 impl Arguments {
-    pub fn new() -> Self {
-        Arguments {
-            show_hidden: false,
-            perm: false,
-            date: false,
-            size: false,
-            no_ordering: false,
-        }
+    pub fn show_hidden(&self) -> bool {
+        self.show_hidden
     }
-    pub fn analyze_entries(&self, path: &str) -> Vec<DirEntry> {
-        let entries: Vec<DirEntry> = match fs::read_dir(path) {
-            Ok(entries) => match self.show_hidden {
-                true => entries.filter_map(|entry| entry.ok()).collect(),
-                false => {
-                    let mut result: Vec<DirEntry> = entries.filter_map(|entry| entry.ok()).collect();
-                    let mut i: usize = 0;
-                    while i < result.len() {
-                        let entry = &result[i];
-                        if let Some(name) = entry.file_name().to_str() {
-                            if name.starts_with('.') {
-                                result.remove(i);
-                                continue;
-                            }
-                        }
-                        i += 1;
+    pub fn permissions(&self) -> bool {
+        self.perm
+    }
+    pub fn date(&self) -> bool {
+        self.date
+    }
+    pub fn size(&self) -> bool {
+        self.size
+    }
+    pub fn no_order(&self) -> bool {
+        self.no_order
+    }
+    pub fn force_print_as_lines(&self) -> bool {
+        self.force_print_as_lines   
+    }
+    fn analyze_entries(&self, path: &str) -> Vec<DirEntry> {
+        let entries: Vec<DirEntry> = match read_dir(path) {
+            Ok(entries) if self.show_hidden => entries.filter_map(|entry| entry.ok()).collect(),
+            Ok(entries) => entries
+                .filter_map(|entry| {
+                    if entry
+                        .as_ref()
+                        .expect("")
+                        .file_name()
+                        .to_string_lossy()
+                        .starts_with('.')
+                    {
+                        None
+                    } else {
+                        entry.ok()
                     }
-                    result
-                }
-            },
+                })
+                .collect(),
             Err(e) => {
-                error("Can't read directory", &e.to_string());
-                std::process::exit(1);
+                std::process::exit(error("Can't read dir", &e.to_string()));
             }
         };
-        match self.no_ordering {
-            true => entries,
-            false => self.order_files(entries),
-        }
-    }
-    fn order_files(&self, mut entries: Vec<DirEntry>) -> Vec<DirEntry> {
-        entries.sort_by_cached_key(|entry| {
-            let file_type = entry.file_type().expect("");
-            let is_dir = file_type.is_dir();
-            let is_symlink = file_type.is_symlink();
-            let is_hidden = entry
-                .path()
-                .file_name()
-                .map_or(false, |name| name.to_string_lossy().starts_with('.'));
-            let priority = match (is_dir, is_symlink, is_hidden) {
-                (true, _, false) => 0,
-                (_, true, false) => 1,
-                (_, _, false) => 2,
-                (true, _, true) => 3,
-                (_, true, true) => 4,
-                (_, _, true) => 5,
-            };
-            (
-                priority,
-                entry
-                    .path()
-                    .file_name()
-                    .unwrap()
-                    .to_string_lossy()
-                    .to_string(),
-                is_hidden,
-            )
-        });
         entries
     }
+
     fn parse_path(&self, mut path: String) -> String {
         if !path.ends_with('/') {
             path.push('/');
         }
         path
     }
+
     fn parse_arguments(&mut self, argument: &str) {
-        match VALID_ARGUMENTS.iter().position(|&x| x == argument) {
-            Some(0) => helper(),
-            Some(1) => self.date = true,
-            Some(2) => self.no_ordering = true,
-            Some(3) => self.perm = true,
-            Some(4) => self.size = true,
-            Some(5) => self.show_hidden = true,
-            _ => match VALID_ABREVIATIONS.iter().position(|&x| x == argument) {
-                Some(0) => helper(),
-                Some(1) => self.date = true,
-                Some(2) => self.no_ordering = true,
-                Some(3) => self.perm = true,
-                Some(4) => self.size = true,
-                Some(5) => self.show_hidden = true,
-                Some(6) => {
-                    self.perm = true;
-                    self.date = true;
-                    self.show_hidden = true;
-                    self.size = true;
-                }
-                _ => error(
-                    "Invalid argument",
-                    &format!(
-                        "type '{} {}' to see all the arguments",
-                        PROGRAM_NAME, VALID_ARGUMENTS[0]
-                    ),
-                ),
-            },
+        match argument {
+            "--help"        | "-h"  => helper(),
+            "--version"     | "-v"  => version(),
+            "--date"        | "-d"  => self.date = true,
+            "--no-order"    | "-no" => self.no_order = true,
+            "--permissions" | "-p"  => self.perm = true,
+            "--size"        | "-s"  => self.size = true,
+            "--show-hidden" | "-sh" => self.show_hidden = true,
+            "--lines"       | "-l"  => self.force_print_as_lines = true,
+            "--all"         | "-a"  => {
+                self.date = true;
+                self.perm = true;
+                self.size = true;
+                self.show_hidden = true;
+            }
+            _ => std::process::exit(error(
+                "Invalid argument",
+                "to see the valid arguments type lis --help",
+            )),
         }
     }
-    pub fn interpreter(mut self) -> (Arguments, String) {
-        let command_arguments = env::args().skip(1);
-        let mut path: String = env::current_dir().unwrap().to_str().unwrap().to_owned();
+
+    pub fn interpreter() -> (Arguments, Vec<DirEntry>) {
+        let mut arguments = Arguments::default();
+        let command_arguments = args().skip(1);
+        let mut path: String = current_dir().unwrap().to_str().unwrap().to_owned();
         for argument in command_arguments {
             match !argument.starts_with('-') {
-                true => path = self.parse_path(argument),
-                false => self.parse_arguments(&argument),
+                true => path = arguments.parse_path(argument),
+                false => arguments.parse_arguments(&argument.to_lowercase()),
             }
         }
-        (self, path)
+        let entries = arguments.analyze_entries(&path);
+        (arguments, entries)
     }
 }
